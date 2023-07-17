9e05f7f64cf0c81ece7a38a654ace95319c0dca1
Marco <tignanellimarco@gmail.com>
2023-07-17 16:08:02 -0300

    add script loading

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,19 @@
-import Map from "./components/Map"
+import { useLoadScript } from "@react-google-maps/api"
+import Map from "./components/Map/Map"
+
+const { VITE_API_KEY } = import.meta.env
 
 function App() {
-  return (
-    <>
-      <Map />
-    </>
-  )
+  // Loads the script for the google map
+  const { isLoaded } = useLoadScript({
+    googleMapsApiKey: VITE_API_KEY,
+    libraries: ["places"]
+  })
+
+  // While loading, shows something else
+  if (!isLoaded) return <div>Loading</div>
+  
+  return <Map />
 }
 
 export default App
