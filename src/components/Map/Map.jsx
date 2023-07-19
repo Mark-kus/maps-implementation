9650160f4804b1b9186b6a6c4f1f57bc71522768
Marco <tignanellimarco@gmail.com>
2023-07-19 16:33:21 -0300

    center CenterMarker

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -117,7 +117,7 @@ export default function Map() {
                     </>      
                 )} 
 
-            {/* <CenterMarker />    */}
+            <CenterMarker />   
             <BlockMap setMoreOptions={setMoreOptions} />
             <GoToMaps />
             </GoogleMap>
