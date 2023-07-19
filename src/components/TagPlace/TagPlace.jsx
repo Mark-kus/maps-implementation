60683ed4da8777a1f8c2f5f94c09a52109fcff0a
Marco <tignanellimarco@gmail.com>
2023-07-19 18:25:00 -0300

    make tag functionality with an input

--- src/components/TagPlace/TagPlace.jsx
+++ src/components/TagPlace/TagPlace.jsx
@@ -1,63 +1,30 @@
-import { useState, useRef, useEffect } from "react";
+import { useState } from "react";
 import "./TagPlace.css"
 
 export default function TagPlace({ onTag }) {
-
   const [inputValue, setInputValue] = useState("");
-  const inputRef = useRef(null);
-  const containerRef = useRef(null);
 
   const handleKeyPress = (e) => {
-    if (e.key === "Enter") {
-      // e.preventDefault();
-      inputRef.current.blur()
-      if (inputValue.length) {
-        onTag(inputValue);
-        setInputValue(null)
-      }
+    // If key is Enter, executes onTag function
+    if (e.key === "Enter" && inputValue.length) {
+      onTag(inputValue);
+      setInputValue("")
     }
   };
 
   const handleChange = (e) => {
-    setInputValue(e.target.textContent.trim());
+    // Makes the input controlled
+    setInputValue(e.target.value);
   };
 
-  useEffect(() => {
-    const range = document.createRange();
-    const sel = window.getSelection();
-    range.selectNodeContents(inputRef.current);
-    range.collapse(false);
-    sel.removeAllRanges();
-    sel.addRange(range);
-    inputRef.current.focus();
-  }, [inputValue]);
-
-  const handleBlur = () => {
-    if (inputValue.trim() === "") {
-      // Si el contenido está vacío al perder el foco, se restaura el placeholder
-      containerRef.current.classList.add("placeholder");
-    } else {
-      containerRef.current.classList.remove("placeholder");
-    }
-  }
-
   return (
-    <div className="tag-place-container" ref={containerRef}>
-
-      <div
-        ref={inputRef}
-        contentEditable
-        suppressContentEditableWarning
-        // type="text"
-        // value={inputValue}
-        onInput={handleChange}
-        onKeyDown={handleKeyPress}
-        className={`label-input ${inputValue ? "" : "placeholder"}`}
-        onBlur={handleBlur}
-        data-placeholder="Etiqueta el lugar aqui"
-      >
-        {inputValue}
-      </div>
-    </div>
-  );
+    <input
+      type="text"
+      value={inputValue}
+      placeholder="Etiqueta el lugar aqui"
+      className="label-input"
+      onChange={handleChange}
+      onKeyDown={handleKeyPress}
+    />
+  )
 }
