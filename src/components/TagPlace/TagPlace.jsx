a3f9f2e14ab6a35abf599601cd05f63b671c2737
Oda2022 <odalismallqui@gmail.com>
2023-07-20 10:12:02 -0500

    input v2.0 :P

--- src/components/TagPlace/TagPlace.jsx
+++ src/components/TagPlace/TagPlace.jsx
@@ -1,8 +1,10 @@
-import { useState } from "react";
+import { useRef, useState, useEffect } from "react";
 import "./TagPlace.css"
 
 export default function TagPlace({ onTag }) {
   const [inputValue, setInputValue] = useState("");
+  const [originalSize, setOriginalSize] = useState(10);
+  const inputRef = useRef();
 
   const handleKeyPress = (e) => {
     // If key is Enter, executes onTag function
@@ -14,7 +16,43 @@ export default function TagPlace({ onTag }) {
 
   const handleChange = (e) => {
     // Makes the input controlled
+
     setInputValue(e.target.value);
+  };
+
+  useEffect(() => {
+    setOriginalSize(inputValue.length || 10);
+  }, [inputValue]);
+
+  useEffect(() => {
+    // When the component unmounts, reset the input size to its original state
+    return () => {
+      const input = inputRef.current;
+      if (input) {
+        input.style.width = "auto";
+        input.setAttribute("size", originalSize); // Set the initial size of the input
+      }
+    };
+  }, [originalSize]);
+
+  useEffect(() => {
+    // Resize the input after rendering
+    resizeInput();
+  });
+
+  const resizeInput = () => {
+    const input = inputRef.current;
+    if (input) {
+      
+      input.setAttribute("size", input.value.length || originalSize);
+
+      const maxWidth = 150; // Modify this value as desired
+      if (input.offsetWidth > maxWidth) {
+        input.style.width = maxWidth + "px";
+      } else {
+        input.style.width = "auto";
+      }
+    }
   };
 
   return (
@@ -25,6 +63,7 @@ export default function TagPlace({ onTag }) {
       className="label-input"
       onChange={handleChange}
       onKeyDown={handleKeyPress}
+      ref={inputRef}
     />
   )
 }
