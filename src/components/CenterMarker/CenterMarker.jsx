eecd84c2bf6a8b316171adb3ec0618fdc7427cfb
Marco <tignanellimarco@gmail.com>
2023-07-24 18:34:54 -0300

    change center marker image

--- src/components/CenterMarker/CenterMarker.jsx
+++ src/components/CenterMarker/CenterMarker.jsx
@@ -30,9 +30,9 @@ export default function CenterMarker({ size }) {
     return (
         <div className="center-marker" style={{ top: `${top}px`, left: `${left}px` }}>
             <img
-                src="https://maps.google.com/mapfiles/kml/paddle/red-circle.png"
-                width={40}
-                height={40}
+                src="https://developers.google.com/static/maps/documentation/javascript/images/default-marker.png?hl=es"
+                width={26}
+                height={38}
                 alt="Marker at the center of the map" />
         </div>
     )
